13a85c7f25d7b2b95fbae14a3104e8b293c2af1e
Darshan Zend <darshandzend@gmail.com>
2015-10-08 16:13:04 +0530

    Crated permute.go

---
+++ permute.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	in := "abcd"
+	out := make([]byte, 0)
+	l := len(in)
+	used := make([]bool, l)
+	permute(in, out, used, l, 0)
+}
+
+func permute(in string, out []byte, used []bool, l int, lvl int) {
+	if lvl == l {
+		fmt.Println(string(out))
+		return
+	}
+	for i := 0; i < l; i++ {
+		if used[i] == true {
+			continue
+		}
+		out = append(out, in[i])
+		used[i] = true
+		permute(in, out, used, l, lvl+1)
+		used[i] = false
+		out = out[:lvl]
+	}
+
+}
